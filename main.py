89e5ec0524a7945495a0f66e131f7796dc8b3698
Đặng Xuân Vương <vuong.1998@gmail.com>
2020-03-04 01:22:35 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib as mpl
+import matplotlib.patches as mpatches
 from matplotlib import pyplot as plt
 # import pandas as pd
 import pymongo
@@ -9,8 +10,8 @@ from utils import *
 
 
 def main():
-	first_date_string = "2020-02-28 00:00"
-	last_date_string = "2020-02-29 00:00"
+	first_date_string = "2020-03-03 00:00"
+	last_date_string = "2020-03-04 00:00"
 	first = datetime.strptime(first_date_string, '%Y-%m-%d %H:%M')
 	last = datetime.strptime(last_date_string, '%Y-%m-%d %H:%M')
 
@@ -28,11 +29,13 @@ def plot(first, last, plot_signals, weather_db):
 	# fig, (ax, bx) = plt.subplots(2, figsize = (16, 9), sharex = True)
 	fig = plt.figure(figsize = (16, 9))
 	ax = fig.add_axes([0.1, 0.4, 0.6, 0.5])
+	ax.set_ylim([-0.1, 1.1])
 	bx = fig.add_axes([0.1, 0.1, 0.6, 0.2])
+	bx.set_ylim([0, 20])
 
 	for signal in plot_signals:
 		try:
-			signal["kwarg"]["label"] = signal["field"] + ' ' + signal["minValue"] + "-" + signal["maxValue"]
+			signal["kwarg"]["label"] = signal["field"] + ' ' + str(signal["minValue"]) + "-" + str(signal["maxValue"])
 		except:
 			pass
 		tmp_data = weather_db[signal["collection"]].find({"Time": {"$gte": first, "$lt": last}})
@@ -45,14 +48,18 @@ def plot(first, last, plot_signals, weather_db):
 		for i in range(len(data)):
 			time.append(data[i]["Time"])
 			value.append(data[i][signal["field"]])
+			if (value[-1] == 0) and (len(value) > 1):
+				value[-1] = value[-2]
 		if (signal["field"] == "OpenWeatherMap_Temperature"):
 			for i in range(len(value)):
 				value[i] -= 273.15
 		value = normalize(value, signal['minValue'], signal['maxValue'])
-		if (signal["kwarg"]["label"] == "Wunderground_Condition"):
+		if (signal["field"] == "Wunderground_Condition"):
 			continue
 		ax.plot(time, value, **signal["kwarg"])
 
+	# Plot condition
+	des = json.loads(open('weather_types.json').read())
 	tmp_data = weather_db["openweathermap"].find({"Time": {"$gte": first, "$lt": last}})
 	data = []
 	for i in tmp_data:
@@ -63,19 +70,36 @@ def plot(first, last, plot_signals, weather_db):
 	for i in range(len(data)):
 		time.append(data[i]["Time"])
 		value.append(data[i]["OpenWeatherMap_Description"])
-	bx.plot(time, value, label = "OpenWeatherMap_Description")
+	time = np.array(time)
+	value = np.array(value)
+	value_id = []
+	value_color = dict()
+	value_des = dict()
+	for v in value:
+		v = v.replace(' ', '')
+		value_id.append(des[v]["id"])
+		value_color[des[v]["id"]] = des[v]["color"]
+		value_des[des[v]["id"]] = v
+	value_id = np.array(value_id)
+	color_lengend = []
+	for i in range(len(des)):
+		if i not in value_color:
+			continue
+		bx.fill_between(time, 0, 20, where = value_id == [i]*len(value_id), color = value_color[i])
+		color_lengend.append(mpatches.Patch(color = value_color[i], label = value_des[i]))
 
+	plt.rcParams.update({'legend.loc': 'best'})
 	ax.legend(bbox_to_anchor = (1, 1))
-	bx.legend(bbox_to_anchor = (1, 1))
-	# plt.savefig('plot/esp/plot_' + first_date_string.replace(' ', '_') + '.eps', format='eps')
-	# plt.savefig('plot/png/plot_' + first.strftime("%Y-%m-%d") + '.png', format='png')
+	bx.legend(handles = color_lengend, bbox_to_anchor = (1.3, 1))
+	plt.savefig('plot/esp/plot_' + first.strftime("%Y-%m-%d") + '.eps', format='eps')
+	plt.savefig('plot/png/plot_' + first.strftime("%Y-%m-%d") + '.png', format='png')
 	# plt.rcParams.update({'font.size': 6, 'figure.figsize': (16, 9)})
 	plt.title('Weather and Signal Data ' + first.strftime("%Y-%m-%d"))
-	mng = plt.get_current_fig_manager()
+	# mng = plt.get_current_fig_manager()
 	# print(type(mng))
-	mng.resize(*mng.window.maxsize())
-	mng.full_screen_toggle()
-	plt.show()
+	# mng.resize(*mng.window.maxsize())
+	# mng.full_screen_toggle()
+	# plt.show()
 
 def time_key(obj):
 	return obj['Time']
